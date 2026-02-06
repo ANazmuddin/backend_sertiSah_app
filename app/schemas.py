c55adf53079fa0dbc9b0d7a9cd8afe2a766b4bcf
ANazmuddin <anazmuddin072@gmail.com>
2026-02-06 21:12:24 +0700

    update schema

--- app/schemas.py
+++ app/schemas.py
@@ -6,3 +6,14 @@ class VerifyRequest(BaseModel):
 class VerifyResponse(BaseModel):
     status: str
     message: str
+
+class CertificateRequest(BaseModel):
+    name: str
+    nim: str
+    program_studi: str
+    institusi: str
+
+class CertificateResponse(BaseModel):
+    certificate_id: str
+    certificate_hash: str
+    file_path: str
