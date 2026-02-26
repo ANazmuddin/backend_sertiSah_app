d6dcc998f33d7f75463f0133d2dc93bbdbfae8df
ANazmuddin <anazmuddin072@gmail.com>
2026-02-26 21:05:33 +0700

    update database

--- app/database.py
+++ app/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./admin.db"
+DATABASE_URL = "sqlite:///./sertisah.db"
 
 engine = create_engine(
     DATABASE_URL,
