7898efcd744bfad8c087fc834ec788d7ce94d31c
ANazmuddin <anazmuddin072@gmail.com>
2026-03-01 22:20:00 +0700

    update models

--- app/models.py
+++ app/models.py
@@ -1,13 +1,10 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, Text
 from datetime import datetime
 from app.database import Base
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
 
-class AdminUser(Base):
-    __tablename__ = "admin_users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    password_hash = Column(String)
+Base = declarative_base()
 
 class Certificate(Base):
     __tablename__ = "certificates"
@@ -21,4 +18,22 @@ class Certificate(Base):
     issue_date = Column(String)
     certificate_hash = Column(String, unique=True, index=True)
     blockchain_tx = Column(String, nullable=True)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+class AdminUser(Base):
+    __tablename__ = "admin_users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, nullable=False)
+    password_hash = Column(String, nullable=False)
+    role = Column(String, default="ADMIN")  # <-- TAMBAHAN
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+class AuditLog(Base):
+    __tablename__ = "audit_logs"
+
+    id = Column(Integer, primary_key=True, index=True)
+    admin_id = Column(Integer)
+    action = Column(String)
+    description = Column(Text)
     created_at = Column(DateTime, default=datetime.utcnow)
